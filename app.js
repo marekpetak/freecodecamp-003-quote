2325dc38c98f30efc1eab20991a24ad95086fc17
Marek Petak <marekpetak@kannegiesser.com>
2016-04-21 16:53:56 +0100

    - styled, just need quote text from somewhere ...

--- app.js
+++ app.js
@@ -3,9 +3,43 @@
 
   angular
     .module('App', [])
-    .controller('AppController', ['$scope',
-      function($scope) {
-        
+    .controller('AppController', ['$scope', 'DataService',
+      function($scope, DataService) {
+        $scope.quote = {
+          text: 'empty text',
+          author: 'empty'
+        };
+
+        $scope.newQuote = function() {
+            DataService.getQuote(function(data, error) {
+              if(error){
+                console.log(error);
+              }
+              else {
+                $scope.quote = data;
+              }
+            });
+        }
+      }
+    ])
+    .service('DataService', [
+      function() {
+        var _dataServiceFactory = {};
+
+        function _getQuote(callback) {
+          var test = {
+            text: "bla bla bla bla bla bla bla bla bla",
+            author: "Humpty Dumpty"
+          }
+
+          if(callback){
+            callback(test, null);
+          }
+        }
+
+        _dataServiceFactory.getQuote = _getQuote;
+
+        return _dataServiceFactory;
       }
     ]);
 
